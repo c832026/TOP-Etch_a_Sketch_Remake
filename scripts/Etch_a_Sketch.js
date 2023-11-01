39a253317f842d9d5c2bee3e94d35c8b724a8391
c832026 <c832026@gmail.com>
2023-11-01 13:57:08 +0800

    Add feature of when hover over will change color

--- scripts/Etch_a_Sketch.js
+++ scripts/Etch_a_Sketch.js
@@ -1,5 +1,6 @@
 // Create 16x16 squares
 const etchContainer = document.querySelector('.etch-flex-container');
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement('div');
@@ -7,3 +8,12 @@ for (let i = 0; i < 16; i++) {
         etchContainer.appendChild(square);
     }
 }
+
+// Hover over will change div color to black
+etchContainer.addEventListener('mouseover', (e) => {
+    const hoveredSquare = e.target;
+    if (hoveredSquare.classList == 'etch-flex-container') {
+        return;
+    }
+    hoveredSquare.classList.add('isBlack');
+});
